366e99ade015d3992e4356b5b5f4a5695e0b4fbd
ali jamaldin <ali@happaning.com>
2024-07-14 13:03:58 +0100

    Create URLRequestConvertible Object for getting Flickr users photos for API requests

--- PhotoLister/PhotoLister/Networking/NetworkRequests/PhotosByUsername.swift
+++ PhotoLister/PhotoLister/Networking/NetworkRequests/PhotosByUsername.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Alamofire
+
+struct PhotosByUsernameRequest: URLRequestConvertible {
+    let username: String
+    private let apiKey = "255429a5b00649776b8bd9be7fd216d8"
+    private let baseURL = "https://api.flickr.com/services/rest/"
+    
+    func asURLRequest() throws -> URLRequest {
+        let parameters: [String: String] = [
+            "api_key": apiKey,
+            "method": "flickr.people.getPhotos",
+            "format": "json",
+            "nojsoncallback": "1",
+            "user_id": username,
+            "per_page": "10"
+        ]
+        let url = try baseURL.asURL()
+        var urlRequest = URLRequest(url: url)
+        urlRequest.method = .get
+        urlRequest = try URLEncoding.default.encode(urlRequest, with: parameters)
+        return urlRequest
+    }
+}
