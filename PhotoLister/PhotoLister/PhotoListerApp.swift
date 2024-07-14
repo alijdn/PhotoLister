670b1d1843a6607d858c67b8d4c77c17ca333ca7
ali jamaldin <ali@happaning.com>
2024-07-14 08:46:49 +0100

    Add boiler plate code

---
+++ PhotoLister/PhotoLister/PhotoListerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoListerApp.swift
+//  PhotoLister
+//
+//  Created by ali Jamaldin on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoListerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
