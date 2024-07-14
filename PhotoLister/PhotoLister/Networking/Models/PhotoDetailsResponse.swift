f4b8e4f017a9fa11775908f1df474d2be3860191
ali jamaldin <ali@happaning.com>
2024-07-14 16:55:57 +0100

    Implemented PhotoDetailsResponse model to represent detailed photo information. Created PhotoDetailsRequest class to handle constructing URL requests for photo details.

--- PhotoLister/PhotoLister/Networking/Models/PhotoDetailsResponse.swift
+++ PhotoLister/PhotoLister/Networking/Models/PhotoDetailsResponse.swift
@@ -1,8 +1,52 @@
-//
-//  PhotoDetailsResponse.swift
-//  PhotoLister
-//
-//  Created by ali Jamaldin on 14/07/2024.
-//
-
 import Foundation
+
+struct PhotoDetailsResponse: Decodable {
+    let photo: PhotoDetails
+    let stat: String
+    
+    struct PhotoDetails: Decodable {
+        let id: String
+        let owner: Owner
+        let title: Content
+        let description: Content
+        let dates: Dates
+        let views: String
+        let tags: Tags
+        let urls: Urls
+        
+        struct Owner: Decodable {
+            let nsid: String
+            let username: String
+            let realname: String?
+            let location: String?
+        }
+        
+        struct Content: Decodable {
+            let _content: String
+        }
+        
+        struct Dates: Decodable {
+            let posted: String
+            let taken: String
+            let lastupdate: String
+        }
+        
+        struct Tags: Decodable {
+            let tag: [Tag]
+            
+            struct Tag: Decodable {
+                let raw: String
+                let _content: String
+            }
+        }
+        
+        struct Urls: Decodable {
+            let url: [Url]
+            
+            struct Url: Decodable {
+                let type: String
+                let _content: String
+            }
+        }
+    }
+}
--- PhotoLister/PhotoLister/Networking/NetworkRequests/PhotoDetails.swift
+++ PhotoLister/PhotoLister/Networking/NetworkRequests/PhotoDetails.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Alamofire
+
+struct PhotoDetailsRequest: URLRequestConvertible {
+    private let apiKey = "255429a5b00649776b8bd9be7fd216d8"
+    private let baseURL = "https://api.flickr.com/services/rest/"
+    let photoId: String
+    
+    func asURLRequest() throws -> URLRequest {
+        let parameters: [String: String] = [
+            "api_key": apiKey,
+            "method": "flickr.photos.getInfo",
+            "format": "json",
+            "safe_search": "1",
+            "nojsoncallback": "1",
+            "photo_id": photoId,
+        ]
+        let url = try baseURL.asURL()
+        var urlRequest = URLRequest(url: url)
+        urlRequest.method = .get
+        urlRequest = try URLEncoding.default.encode(urlRequest, with: parameters)
+        
+        print("URL Request: \(urlRequest)")
+        return urlRequest
+    }
+}
