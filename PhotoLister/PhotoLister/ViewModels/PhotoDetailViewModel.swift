afe4a5df9cffa6f653e91b23f236f8cf842b2350
ali jamaldin <ali@happaning.com>
2024-07-14 16:52:14 +0100

    Implemented PhotoDetailViewModel to manage photo details retrieval and state. Integrated network requests and data parsing for fetching photo details.

--- PhotoLister/PhotoLister/ViewModels/PhotoDetailViewModel.swift
+++ PhotoLister/PhotoLister/ViewModels/PhotoDetailViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Combine
+
+class PhotoDetailViewModel: ObservableObject {
+    let networkManager: NetworkManager
+    private var tasks = Set<AnyCancellable>()
+    @Published var photoDetails: PhotoDetailsResponse.PhotoDetails?
+    
+    init(networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+    
+    func getPhotoDetails(photoId: String) {
+        let request = PhotoDetailsRequest(photoId: photoId)
+        
+        networkManager.request(request)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error: \(error)")
+                }
+            } receiveValue: { [weak self] (response: PhotoDetailsResponse) in
+                print("Response: \(response)")
+                self?.photoDetails = response.photo
+            }.store(in: &tasks)
+    }
+}
