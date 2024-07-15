c087ec147491d2cb7ac07f89f843c311712cff0c
ali jamaldin <ali@happaning.com>
2024-07-15 22:02:33 +0100

    Implement search for username and tags, add error handling and pagination
    - Implemented search functionality for both username and tags in PhotoListView.
    - Added error handling to display user-friendly messages for various scenarios.
    - Implemented pagination to load more photos dynamically.

--- PhotoLister/PhotoLister/ViewModels/PhotoListViewModel.swift
+++ PhotoLister/PhotoLister/ViewModels/PhotoListViewModel.swift
@@ -9,18 +9,25 @@ import Foundation
 import Combine
 
 class PhotoListViewModel: ObservableObject {
-    let networkManager: NetworkManager
-    private var tasks = Set<AnyCancellable>()
+    private let networkManager: NetworkManager
+    private var subscriptions = Set<AnyCancellable>()
+    @Published var errorMessage: String?
     
     @Published var photos: [Photo] = []
-    var searchText = "";
+    @Published var tags = Set<String>()
+    
+    @Published var searchOption: SearchOption = .tags
+    @Published var isSearchingUserName: Bool = false
+    var searchText = ""
+    var currentPage = 1
+    var lastPage = 0
     
     init(networkManager: NetworkManager) {
         self.networkManager = networkManager
     }
     
-    func getTaggedPhoto() {
-        let request = PhotosByTagsRequest(tags: "Yorkshire")
+    func getTaggedPhoto(tags: String, page: String) {
+        let request = PhotosByTagsRequest(tags: tags, page: page)
         
         networkManager.request(request)
             .sink { completion in
@@ -29,11 +36,64 @@ class PhotoListViewModel: ObservableObject {
                     break
                 case .failure(let error):
                     print("Error: \(error)")
+                    self.errorMessage = "Failed to load photos with tags '\(tags)'. Please try again."
                 }
             } receiveValue: { [weak self] (response: FlickrResponse) in
+                if self?.lastPage == 0 {
+                    self?.lastPage = response.photos.pages
+                }
+                if self?.currentPage == 1 {
+                    self?.photos = response.photos.photo
+                } else {
+                    self?.photos += response.photos.photo
+                }
+            }.store(in: &subscriptions)
+    }
+    
+    func getPhotosByUsername(owner: String, page: String) {
+        let request = PhotosByUsernameRequest(username: owner, page: page)
+        networkManager.request(request)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    self.errorMessage = nil
+                case .failure(let error):
+                    print("Error: \(error)")
+                    self.errorMessage = "Unable to load photos for user '\(owner)'. Please check the username and try again."
+                    self.photos.removeAll()
+                }
+            } receiveValue: { [weak self] (response: FlickrResponse) in
+                if self?.lastPage == 0 {
+                    self?.lastPage = response.photos.pages
+                }
+                if self?.currentPage == 1 {
+                    self?.photos = response.photos.photo
+                } else {
+                    self?.photos += response.photos.photo
+                }
                 self?.photos = response.photos.photo
-            }.store(in: &tasks)
+            }.store(in: &subscriptions)
+    }
+    
+    func searchPhotos() {
+        switch searchOption {
+        case .tags:
+            isSearchingUserName = false
+            lastPage = 0
+            errorMessage = nil
+            getTaggedPhoto(tags: getTagString, page: "1")
+        case .username:
+            tags.removeAll()
+            isSearchingUserName = true
+            lastPage = 0
+            getPhotosByUsername(owner: searchText.filter { !$0.isWhitespace }, page: "1")
+        }
+    }
+    
+    var getTagString: String {
+        let cleanedTags = searchText.filter { !$0.isWhitespace }
+        tags.insert(cleanedTags)
+        let tagString = tags.map { $0 }.joined(separator: ",")
+        return tagString
     }
 }
-
-
--- PhotoLister/PhotoLister/Views/PhotoListView.swift
+++ PhotoLister/PhotoLister/Views/PhotoListView.swift
@@ -23,28 +23,79 @@ struct PhotoListView: View {
                 .background(Color(.systemGray6))
                 .cornerRadius(8)
                 .padding([.leading, .trailing])
-            //Displaying usericon, username and photo
+                .onSubmit {
+                    viewModel.searchPhotos()
+                }
+            //Picker for tags or username search
+            Picker("Search Option", selection: $viewModel.searchOption) {
+                Text("Tags").tag(SearchOption.tags)
+                Text("Username").tag(SearchOption.username)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding([.leading, .trailing])
+            
+            //Displaying error if no users found
+            if let errorMessage = viewModel.errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            //Displaying tags in a horizontal scroll view if the user is not searching for username
+            if viewModel.isSearchingUserName == false {
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(Array(viewModel.tags), id: \.self) { tag in
+                            TagButton(tag: tag) {
+                                viewModel.tags.remove(tag)
+                                if viewModel.tags.isEmpty {
+                                    viewModel.photos.removeAll()
+                                } else {
+                                    viewModel.getTaggedPhoto(tags: viewModel.getTagString, page: "1")
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
+            //Scroll view for displaying photos
             ScrollView {
                 LazyVStack(spacing: 40) {
                     ForEach(viewModel.photos) { photo in
                         VStack {
+                            //Navigating to a different view displaying user photos
                             NavigationLink(destination: UserPhotosView(username: photo.owner)) {
                                 Header(imageURL: photo.userIconURL, username: photo.owner)
                             }
-                            NavigationLink(destination: PhotoDetailView(photoId: photo.id, photoUrl: photo.photoURL, userIconURL: photo.userIconURL)) {
-                                Cell(imageURL: photo.photoURL)
+                            //Navigating to a different view displaying details about a single photo
+                            NavigationLink(destination: PhotoDetailView(photoId: photo.id, photoUrl: photo.photoURL,userIconURL:photo.userIconURL)) {
+                                    Cell(imageURL: photo.photoURL)
+                                }
+                            }
+                            .onAppear() {
+                                if photo.id == viewModel.photos.last?.id {
+                                    print("Reached the last photo")
+                                    if viewModel.lastPage > viewModel.currentPage {
+                                        viewModel.currentPage += 1
+                                        viewModel.getTaggedPhoto(tags: viewModel.getTagString, page: String(viewModel.currentPage))
+                                    }
+                                }
                             }
                         }
                     }
+                    .padding(5)
                 }
-                .padding(5)
-            }
         }
         .navigationTitle("Search view")
         .onAppear() {
-            viewModel.getTaggedPhoto(tags: ["Yorkshire"])
+            viewModel.tags.insert("Yorkshire")
+            viewModel.getTaggedPhoto(tags: "Yorkshire", page: "1")
         }
     }
+}
+
+enum SearchOption {
+    case tags, username
 }
 
 struct Cell: View {
@@ -75,6 +126,29 @@ struct Header: View {
                 .fontWeight(.bold)
             Spacer()
         }
+    }
+}
+
+struct TagButton: View {
+    let tag: String
+    var action: () -> Void
+
+    var body: some View {
+        HStack {
+            Button(action: action) {
+                Text(tag)
+                    .foregroundColor(.blue)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+            
+                Image(systemName: "xmark.circle")
+                    .foregroundColor(.blue)
+            }
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 5)
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(8)
     }
 }
 
